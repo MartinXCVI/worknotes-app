20e88456248aa75a6dbe9d402ee2b2e4eb2687c1
MartinXCVI <mdzg96@gmail.com>
2024-11-18 00:43:15 -0300

    Added Redux and setup. Added API slices: notesApiSlice.js, usersApiSlice.js // Installed dependencies: reduxjs/toolkit, react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// App import
 import App from './App.jsx'
+// CSS import
 import './css/styles.css'
 // React router imports
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// Redux imports
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
