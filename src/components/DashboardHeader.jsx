76838300d4f1d41a004a56979840e6db88632e32
MartinXCVI <mdzg96@gmail.com>
2024-11-23 23:41:12 -0300

    Progress in login component and logic

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,18 +1,75 @@
+// React hooks
+import { useEffect } from "react"
+
+// Font awesome icons imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+
 // React router imports
-import { Link } from "react-router-dom"
+import { useNavigate, useLocation, Link } from "react-router-dom"
+
+// Redux imports
+import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+// RegEx constants
+const dashRegEx = /^\dashboard(\/)?$/
+const notesRegEx = /^\dashboard\/notes(\/)?$/
+const usersRegEx = /^\dashboard\/users(\/)?$/
+
 
 const DashboardHeader = () => {
+
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const [
+    sendLogout,
+    {
+      isLoading,
+      isSuccess,
+      isError,
+      error
+    }
+  ] = useSendLogoutMutation()
   
+  useEffect(()=> {
+    if(isSuccess) navigate('/')
+  }, [isSuccess, navigate])
+
+  // Logout handler that calls the sendLogout function
+  const handleLogoutClick = ()=> sendLogout()
+
+  if(isLoading) return <p>Logging out...</p>
+
+  if(isError) return <p>Error: {error.data?.message}</p>
+
+  let dashboardClass = null
+  
+  if(!dashRegEx.test(pathname) && !notesRegEx.test(pathname) && !usersRegEx.test(pathname)) {
+    dashboardClass = 'dash-header-container-small'
+  }
+
+  const logoutButton = (
+    <button
+      className='icon-button'
+      title='Logout'
+      onClick={handleLogoutClick}
+    >
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  )
+
   const content = (
     <header className="dashboard-header">
-        <div className="dashboard-header-container">
-            <Link to="/dashboard">
-                <h1 className="dashboard-header-title">workNotes</h1>
-            </Link>
-            <nav className="dashboard-header-nav">
-                {/* add nav buttons later */}
-            </nav>
-        </div>
+      <div className={`dashboard-header-container ${dashboardClass}`}>
+        <Link to="/dashboard">
+          <h1 className="dashboard-header-title">workNotes</h1>
+        </Link>
+        <nav className="dashboard-header-nav">
+          {/* add more buttons later */}
+          {logoutButton}
+        </nav>
+      </div>
     </header>
   )
   return content
--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,8 +1,117 @@
+// React hooks imports
+import { useRef, useState, useEffect } from 'react'
+
+// React router imports
+import { useNavigate, Link } from 'react-router-dom'
+
+// Redux imports
+import { useDispatch } from 'react-redux'
+import { setCredentials } from './authSlice'
+import { useLoginMutation } from './authApiSlice'
+
 
 const Login = () => {
-  return (
-    <h1>Login</h1>
+
+  const userRef = useRef()
+  const errorRef = useRef()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const [login, { isLoading }] = useLoginMutation()
+  
+  /* Puts the focus in the username 
+  field once the components mounts */
+  useEffect(()=> {
+    userRef.current.focus()
+  }, [])
+
+  /* Clears the error message once the 
+  state of the username or password changes */
+  useEffect(()=> {
+    setErrorMsg('')
+  }, [username, password])
+
+  /* Form submission */
+  const handleSubmit = async (event)=> {
+    event.preventDefault()
+    try {
+      const { accessToken } = await login({ username, password}).unwrap()
+      dispatch(setCredentials({ accessToken }))
+      setUsername('')
+      setPassword('')
+      navigate('/dashboard')
+    } catch(error) {
+      if(!error.status) {
+        setErrorMsg('No server response was received')
+      } else if(error.status === 400) {
+        setErrorMsg('Missing either username or password')
+      } else if(error.status === 401) {
+        setErrorMsg('Unauthorized')
+      } else {
+        setErrorMsg(error.data?.message)
+      }
+      errorRef.current.focus()
+    }
+  }
+
+  const handleUserInput = event => setUsername(event.target.value)
+  const handlePassInput = event => setPassword(event.target.value)
+
+  const errorClass = errorMsg ? 'error-msg' : 'offscreen'
+
+  if(isLoading) return <p>Loading...</p>
+
+  const content = (
+    <section className='public'>
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+      <main className='login'>
+        {/* Error message */}
+        <p 
+          ref={errorRef} 
+          className={errorClass}
+          aria-live='assertive'
+        >
+          {errorMsg}
+        </p>
+        <form className='form' onSubmit={handleSubmit}>
+          {/* Username input */}
+          <label htmlFor="username">Username:</label>
+          <input 
+            className='form-input'
+            type="text"
+            id='username'
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete='off'
+            required
+          />
+          {/* Password input */}
+          <label htmlFor="password">Password:</label>
+          <input
+            className='form-input'
+            type='password'
+            id='password'
+            onChange={handlePassInput}
+            value={password}
+            required
+          />
+          <button className='form-submit-button'>Sign In</button>
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
   )
+
+  return content
 }
 
 export default Login
