3a283b427ad401840b5ba49394c009cf2d33a464
MartinXCVI <mdzg96@gmail.com>
2024-11-26 18:01:06 -0300

    Progress in headers' icons and navigation. Little fix in regular expressions

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 
 // Font awesome icons imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import { faRightFromBracket, faUserPlus, faUserGear, faFilePen, faFileCirclePlus } from "@fortawesome/free-solid-svg-icons"
 
 // React router imports
 import { useNavigate, useLocation, Link } from "react-router-dom"
@@ -11,13 +11,18 @@ import { useNavigate, useLocation, Link } from "react-router-dom"
 // Redux imports
 import { useSendLogoutMutation } from '../features/auth/authApiSlice'
 
+// Custom hooks imports
+import useAuth from '../hooks/useAuth'
+
 // RegEx constants
-const dashRegEx = /^\dashboard(\/)?$/
-const notesRegEx = /^\dashboard\/notes(\/)?$/
-const usersRegEx = /^\dashboard\/users(\/)?$/
+const dashRegEx = /^\/dashboard(\/)?$/
+const notesRegEx = /^\/dashboard\/notes(\/)?$/
+const usersRegEx = /^\/dashboard\/users(\/)?$/
 
 
 const DashboardHeader = () => {
+
+  const { isManager, isAdmin } = useAuth()
 
   const navigate = useNavigate()
   const { pathname } = useLocation()
@@ -36,20 +41,75 @@ const DashboardHeader = () => {
     if(isSuccess) navigate('/')
   }, [isSuccess, navigate])
 
+  const handleNewNoteClick = ()=> navigate('/dashboard/notes/new')
+  const handleNewUserClick = ()=> navigate('/dashboard/users/new')
+  const handleNotesClick = ()=> navigate('/dashboard/notes')
+  const handleUsersClick = ()=> navigate('/dashboard/users')
+
   // Logout handler that calls the sendLogout function
   const handleLogoutClick = ()=> {
     sendLogout()
     navigate('/')
   }
 
-  if(isLoading) return <p>Logging out...</p>
-
-  if(isError) return <p>Error: {error.data?.message}</p>
-
   let dashboardClass = null
   
   if(!dashRegEx.test(pathname) && !notesRegEx.test(pathname) && !usersRegEx.test(pathname)) {
     dashboardClass = 'dash-header-container-small'
+  }
+
+  let newNoteButton = null
+  if (notesRegEx.test(pathname)) {
+    newNoteButton = (
+      <button
+        className="icon-button"
+        title="New Note"
+        onClick={handleNewNoteClick}
+      >
+        <FontAwesomeIcon icon={faFileCirclePlus} />
+      </button>
+    )
+  }
+
+  let newUserButton = null
+  if (usersRegEx.test(pathname)) {
+    newUserButton = (
+      <button
+        className="icon-button"
+        title="New User"
+        onClick={handleNewUserClick}
+      >
+        <FontAwesomeIcon icon={faUserPlus} />
+      </button>
+    )
+  }
+
+  let userButton = null
+  if (isManager || isAdmin) {
+    if (!usersRegEx.test(pathname) && pathname.includes('/dashboard')) {
+      userButton = (
+        <button
+          className="icon-button"
+          title="Users"
+          onClick={handleUsersClick}
+        >
+          <FontAwesomeIcon icon={faUserGear} />
+        </button>
+      )
+    }
+  }
+
+  let notesButton = null
+  if (!notesRegEx.test(pathname) && pathname.includes('/dashboard')) {
+    notesButton = (
+      <button
+        className="icon-button"
+        title="Notes"
+        onClick={handleNotesClick}
+      >
+        <FontAwesomeIcon icon={faFilePen} />
+      </button>
+    )
   }
 
   const logoutButton = (
@@ -62,18 +122,37 @@ const DashboardHeader = () => {
     </button>
   )
 
+  const errorClass = isError ? 'error-msg' : 'offscreen'
+
+  let buttonContent
+  if(isLoading) {
+    buttonContent = <p>Logging Out...</p>
+  } else {
+    buttonContent = (
+      <>
+        {newNoteButton}
+        {newUserButton}
+        {notesButton}
+        {userButton}
+        {logoutButton}
+      </>
+    )
+  }
+
   const content = (
-    <header className="dashboard-header">
-      <div className={`dashboard-header-container ${dashboardClass}`}>
-        <Link to="/dashboard">
-          <h1 className="dashboard-header-title">workNotes</h1>
-        </Link>
-        <nav className="dashboard-header-nav">
-          {/* add more buttons later */}
-          {logoutButton}
-        </nav>
-      </div>
-    </header>
+    <>
+      <p className={errorClass}>{error?.data?.message}</p>
+      <header className="dashboard-header">
+        <div className={`dashboard-header-container ${dashboardClass}`}>
+          <Link to="/dashboard">
+            <h1 className="dashboard-header-title">workNotes</h1>
+          </Link>
+          <nav className="dashboard-header-nav">
+            {buttonContent}
+          </nav>
+        </div>
+      </header>
+    </>
   )
   return content
 }
